f32d2b3464448390095546d998686193ef5a3cb9
Angela Armstorfer <armstorfer@silverserver.at>
2018-02-05 11:31:04 +0100

    second exercise

---
+++ exercise_2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="style.css">
+	<title>php uebungen</title>
+</head>
+<body>
+
+<?php
+if(date('D') == 'Mon') { 
+  echo "Happy Monday!";
+} else {
+  echo "Have a nice day!";
+}
+?>
+
+</body>
+</html>
