0e45b3d5afb77c47d55ffd49d852c50362b1d992
Angela Armstorfer <armstorfer@silverserver.at>
2018-02-05 10:45:46 +0100

    first

---
+++ exercise_day_1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="style.css">
+	<title>php uebungen</title>
+</head>
+<body>
+
+	<?php
+		
+		$myName = "Angela";
+		echo "<h1>".$myName."</h1>"
+	?>
+
+</body>
+</html>
